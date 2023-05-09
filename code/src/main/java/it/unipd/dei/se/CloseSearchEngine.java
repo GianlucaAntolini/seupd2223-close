9af15f1d6fbd6be3d51db4dc7619162d0e770894
Seyedreza Safavi <Seyedrseyedreza.safavi@studenti.unipd.it>
2023-05-09 09:30:51 +0200

    add query expansion result with llm

--- code/src/main/java/it/unipd/dei/se/CloseSearchEngine.java
+++ code/src/main/java/it/unipd/dei/se/CloseSearchEngine.java
@@ -118,7 +118,7 @@ public class CloseSearchEngine {
                 runPath,
                 maxDocsRetrieved,
                 false,
-                "all-MiniLM-L6-v2"
+                null
         );
         searcher.search();
 
